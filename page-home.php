b508339657f92e6453098f635ddef7a671c8616f
qulpe1 <adrian_tudor_01@yahoo.com>
2020-11-26 22:08:41 +0100

    homepage animations - need content
    animations and pagination for desktop ready - need content

--- page-home.php
+++ page-home.php
@@ -4,10 +4,15 @@ if (have_posts()) :
     while (have_posts()) : the_post(); ?>
 
     <div class="page-container">
-        <div class="emotionz"></div>
+        <div class="emotionz"></div> 
+
+        <div class="sparkles"><div class="sparkles" id="spark-1"><div class="sparkles" id="spark-1"><div class="sparkles" id="spark-1"></div></div></div></div>
+      
+        <div class="spotlight" id="spotlight"></div>
+        
         <div class="home-artist"><div id="skew"></div></div>
     </div>
-
+    
 <?php endwhile;
 else :
     echo ' <p>no content found</p>';
