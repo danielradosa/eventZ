190dcb5ce8e702d2343459a2de8a65b6cfdf32d5
Penda Djunior Kanime Svane <thesvaneman@gmail.com>
2020-11-27 18:18:13 +0100

    Updated About Page
    -> Added styling and html tags for group members

--- page-about.php
+++ page-about.php
@@ -8,32 +8,41 @@
                 <img id="eventZGroupImage" src="https://barbaraiweins.com/wp-content/uploads/2020/07/Success-Party.jpg" alt="">
             </div>
             <div class="eventZProject">
-                A group of 4 looking to make your time finding the right night event much easier. 
-                We are based in Esbjerg and created this off the chain Website with collaboration of our Educational Institution.
+                A group of 4 Multimedia Designers. <br>
+                Our Goal is to help you find the right event a lot easier. <br>
+                We are based in Esbjerg and created this off the chain Website with collaboration with our Educational Institution - EASV.
             </div>
-            <div class="eventZProjectLinks">
-                <div id="eventZProjectLinksTitle">
+             <div id="eventZProjectLinksTitle">
                     Find us Online @
-                </div>
-                
+            </div>
+            <div class="eventZProjectLinks">                
                 <div id="groupMember">
                     Adi Tudor 
+                    <br>
                     <a href="">GitHub</a>
+                    <br>
                     <a href="">LinkedIn</a>
                 </div>
                 <div id="groupMember">
-                    Daniel Radosa
+                    Daniel Radosa 
+                    <br>
                     <a href="">GitHub</a>
+                    <br>
                     <a href="">LinkedIn</a>
                 </div>
                 <div id="groupMember">
-                    Oliwia Owczarczyk
+                    Oliwia Owczarczyk 
+                    <br>
                     <a href="">GitHub</a>
+                    <br>
                     <a href="">LinkedIn</a>
                 </div>
                 <div id="groupMember">
-                    Penda Svane
+                    Penda Svane 
+                    <br>
+
                     <a href="https://github.com/TheSvaneMan">GitHub</a>
+                    <br>
                     <a href="https://www.linkedin.com/in/thesvaneman/">LinkedIn</a>
                 </div>
             </div>
