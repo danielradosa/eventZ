b4b3f6f4c3457bf49a987c3e656e9e6225158aba
Penda Djunior Kanime Svane <thesvaneman@gmail.com>
2020-11-27 17:15:38 +0100

    Updated events + about pages
    -> Updated mobile version of about page
    -> Updated events page

---
+++ page-about.php
@@ -0,0 +1,50 @@
+<?php get_header(); ?>
+    <div id="aboutUsPage">
+        <div class="eventZPanel">
+            <div id="eventZMembers">
+                <div id="eventZMembersTitle">
+                    Who We Are
+                </div>
+                <img id="eventZGroupImage" src="https://barbaraiweins.com/wp-content/uploads/2020/07/Success-Party.jpg" alt="">
+            </div>
+            <div class="eventZProject">
+                A group of 4 looking to make your time finding the right night event much easier. 
+                We are based in Esbjerg and created this off the chain Website with collaboration of our Educational Institution.
+            </div>
+            <div class="eventZProjectLinks">
+                <div id="eventZProjectLinksTitle">
+                    Find us Online @
+                </div>
+                <div class="groupMembers">
+                    <div id="groupMember">
+                    Adi Tudor 
+                    <a href="">GitHub</a>
+                    <a href="">LinkedIn</a>
+                </div>
+                <div id="groupMember">
+                    Daniel Radosa
+                    <a href="">GitHub</a>
+                    <a href="">LinkedIn</a>
+                </div>
+                <div id="groupMember">
+                    Oliwia Owczarczyk
+                    <a href="">GitHub</a>
+                    <a href="">LinkedIn</a>
+                </div>
+                <div id="groupMember">
+                    Penda Svane
+                    <a href="https://github.com/TheSvaneMan">GitHub</a>
+                    <a href="https://www.linkedin.com/in/thesvaneman/">LinkedIn</a>
+                </div>
+                </div>
+                
+            </div>
+            
+            
+        </div>
+        <div id="gradientElement1">
+
+        </div>
+    </div>
+
+<?php get_footer(); ?>
---
+++ page-events.php
@@ -0,0 +1,215 @@
+<?php get_header(); ?>
+    <div id="eventsPage">
+        <div id="eventsTitle">
+            <div id="eventsPageTitle">
+                    BIGGEST CREATORS
+            </div>
+            <div id="eventsPageSubTitle">
+                ALL IN ONE PLACE
+            </div>
+        </div>
+        <!-- left Column --> 
+        <div class="eventsPageColumnOne">
+            <!-- Events Row --> 
+            <div class="artistBlockRow">
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Events Row End -->
+            <!-- Events Row --> 
+            <div class="artistBlockRow">
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Events Row End -->
+             <!-- Events Row --> 
+            <div class="artistBlockRow">
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Events Row End -->
+             <!-- Events Row --> 
+            <div class="artistBlockRow">
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+                <div id="artistBlock">
+                    <div id="artistEventDate">
+                        <p>3rd September</p>
+                    </div>
+                    <div id="artistImage">
+                        <img src="https://patch.com/img/cdn20/users/22895579/20160830/022750/styles/raw/public/article_images/metlife_stadium_fireworks_edit-1472580367-2792.jpg" width="300px" alt="">
+                    </div>
+                    <div id="artistName">
+                        <h3>ice peak</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Events Row End -->
+    </div>
+    <!-- right Column --> 
+     <div class="eventsPageColumnTwo">
+        <div class="artistEventSidePanel">
+              <div class="artistGenre">
+                  <div id="genreName">
+                      <h1>Rock</h1>
+                  </div>
+                  <div id="artistNameGenre">
+                      <p>Barack Obana</p>
+                      <p>John Lennon</p>
+                  </div>
+              </div>  
+              <div class="artistGenre">
+                  <div id="genreName">
+                      <h1>Pop</h1>
+                  </div>
+                  <div id="artistNameGenre">
+                      <p>Barack Obana</p>
+                      <p>John Lennon</p>
+                  </div>
+              </div> 
+              <div class="artistGenre">
+                  <div id="genreName">
+                      <h1>Hip-Hop</h1>
+                  </div>
+                  <div id="artistNameGenre">
+                      <p>Barack Obana</p>
+                      <p>John Lennon</p>
+                  </div>
+              </div> 
+               <div class="artistGenre">
+                  <div id="genreName">
+                      <h1>Indie</h1>
+                  </div>
+                  <div id="artistNameGenre">
+                      <p>Barack Obana</p>
+                      <p>John Lennon</p>
+                  </div>
+              </div> 
+               <div class="artistGenre">
+                  <div id="genreName">
+                      <h1>R&B</h1>
+                  </div>
+                  <div id="artistNameGenre">
+                      <p>Barack Obana</p>
+                      <p>John Lennon</p>
+                  </div>
+              </div> 
+        </div>
+    </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
