e237efd556382a93eaf341c36524acd12f175fba
Penda Djunior Kanime Svane <thesvaneman@gmail.com>
2020-11-28 15:37:30 +0100

    Updated wp-Menus
    -> Added in Post Category feature to allow seperate pages displaying different posts related to the page.  Allow for management of Artist Posts and Event Posts.
    
    -> Updated wp-Menu to display links
    -> Updated Style.css
    -> Updated the functions.php
    
    -> Updated Wordpress permalinks on Local Client

--- index.php
+++ index.php
@@ -1,43 +1,41 @@
-<?php get_header();
+<?php
+/**
+ * The template for displaying the home/index page.
+ * This template will also be called in any case where the Wordpress engine
+ * doesn't know which template to use (e.g. 404 error)
+ */
+get_header(); // This fxn gets the header.php file and renders it
+?>
 
-if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
+<div class="main">
+    <?php if (have_posts()): ?>
+    	<?php while (have_posts()): the_post()?>
+					<div class="post">
+						<div class="postTitle">
+							<h1>
+								<a href="<?php the_permalink()?>"><?php the_title()?></a>
+							</h1>
+							<hr>
+									<span><?php the_date()?> at <?php the_title()?></span>
+						</div>
+						<div class="postContent">
+							<p><?php the_content()?></p>
+						</div>
+					</div>
+				<?php endwhile;?>
+    <?php endif;?>
+</div>
+<div class="pageNavigation">
+	<hr>
+    <button class="postNavigationPrev">
+        <?php previous_posts_link("Previous Posts")?>
+	</button>
+	<hr>
+    <button  class="postNavigationNext">
+        <?php next_posts_link("Newer Posts")?>
+	</button>
+	<hr>
+	
+</div>
+<?php get_footer(); // This fxn gets the footer.php file and renders it ?>
 
-
-    <div class="post">
-        <h1>
-            <a href="<?php the_permalink() ?>">
-                <?php the_title() ?>
-            </a>
-        </h1>
-        <span>
-            <div class="content">
-                <<?php if($post->post_excerpt){ ?>
-        <p>
-            <?php echo get_the_excerpt(); ?>
-            <a href="<?php the_permalink(); ?>">
-                Read the post&raquo;
-            </a>
-        </p>
-                <?php the_post_thumbnail('small-thumbnail'); ?>
-            </div>
-            <span class="post__author"><?php the_author() ?></span>
-            <br>
-            <span class="post__date">
-                <?php the_date() ?> @ <?php the_time() ?> 
-            </span>
-        </span>
-    </div>
-    
-
-
-<?php } else {
-    the_content();
-} ?>
-<?php endwhile;
-else :
-    echo ' <p>no content found</p>';
-
-endif;
-
-get_footer(); ?>
